d025ac9e1f67058decdaaa4b6292f9d815a7c597
BadAimWeeb <badaimweeb@protonmail.com>
2026-03-13 22:00:14 -0500

    fix centrality score

--- src/components/Centrality.tsx
+++ src/components/Centrality.tsx
@@ -3,6 +3,11 @@ import { NodeTables, SCtoRCLookup } from "./NodeTables";
 import cytoscape from "cytoscape";
 import { Table } from "@radix-ui/themes";
 
+// weight pulled from map.dn42
+const alpha = 0.5;
+const beta = 0.3;
+const gamma = 0.2;
+
 export default function Centrality(props: {
     formattedTopology: Record<string, Record<string, [nhRTT: number | null, nh: string | null]>>;
     dynamicRgCode: Record<string, string>;
@@ -96,15 +101,56 @@ export default function Centrality(props: {
             }
         }
 
+        //@ts-ignore
+        let dcn: cytoscape.SearchDegreeCentralityNormalizedResultDirected = cy.elements().degreeCentralityNormalized({
+            directed: true
+        });
+        //@ts-ignore
+        let dcnu: cytoscape.SearchDegreeCentralityNormalizedResultUndirected = cy.elements().degreeCentralityNormalized({
+            directed: false
+        });
+
+        let bcn = cy.elements().betweennessCentrality({
+            directed: true
+        });
         let ccn = cy.elements().closenessCentralityNormalized({
             directed: true
         });
 
         let sortedNodes = [...cy.nodes()].map(n => ({
             id: n.id(),
+
             ccn: ccn.closeness(n),
-            connCandidates: kc.get(n.id()) || new Map<string, number>()
-        })).sort((a, b) => b.ccn - a.ccn);
+            bcn: bcn.betweenness(n),
+            dcn: dcnu.degree(n),
+            dcnIn: dcn.indegree(n),
+            dcnOut: dcn.outdegree(n),
+            index: 0, // to be filled later
+
+            connCandidates: kc.get(n.id()) || new Map<string, number>(),
+            recvCandidates: new Set<string>()
+        })).sort((a, b) => b.bcn - a.bcn);
+
+        let maxBcn = sortedNodes[0]?.bcn || 1;
+        for (let node of sortedNodes) {
+            // normalize
+            node.bcn /= maxBcn;
+
+            node.index = (alpha * node.bcn) + (beta * node.ccn) + (gamma * node.dcn);
+        }
+
+        sortedNodes.sort((a, b) => b.index - a.index);
+        
+        let mapping: Record<string, typeof sortedNodes[number]> = {};
+        for (let node of sortedNodes) {
+            mapping[node.id] = node;
+        }
+
+        for (let node of sortedNodes) {
+            for (let target of node.connCandidates.keys()) {
+                mapping[target].recvCandidates.add(node.id);
+            }
+        }
 
         cy.destroy();
 
@@ -131,9 +177,16 @@ export default function Centrality(props: {
             }
 
             let downstream = new Set<string>();
+            let downstreamPeers = new Set<string>();
             for (let [did, upstreamSet] of fc.entries()) {
                 if (upstreamSet.has(n.id)) {
                     downstream.add(did);
+                }
+            }
+
+            for (let pid of n.recvCandidates) {
+                if (!downstream.has(pid)) {
+                    downstreamPeers.add(pid);
                 }
             }
 
@@ -143,17 +196,33 @@ export default function Centrality(props: {
                     {SCtoRCLookup[n.id]?.[1] || n.id}
                     {SCtoRCLookup[n.id] ? <><br /><span style={{ fontSize: 10 }}>{n.id} / {SCtoRCLookup[n.id][0]}</span></> : ""}
                 </Table.Cell>
-                <Table.Cell>{n.ccn.toFixed(4)}</Table.Cell>
+                <Table.Cell>
+                    {(n.index * 10000).toFixed(0)}<br />
+                </Table.Cell>
+                <Table.Cell style={{ whiteSpace: "pre", fontFamily: "monospace" }}>
+                    BCN: {n.bcn.toFixed(4)}<br />
+                    CCN: {n.ccn.toFixed(4)}<br />
+                    DCN: {n.dcn.toFixed(4)}
+                </Table.Cell>
                 {!!props.showDownstream && <Table.Cell>
+                    Degree (in): {n.dcnIn.toFixed(4)}<br /><br />
                     Downstream: {downstream.size} {downstream.size > 0 ? <><br />{
                         Array.from(downstream).map(target => (
                             `${SCtoRCLookup[target]?.[1] || target}^`
                         )).join(",  ")
-                    }</> : ""}<br />
+                    }</> : ""}
                     <br />
+                    <br />
+                    Peer: {downstreamPeers.size} {downstreamPeers.size > 0 ? <><br />{
+                        Array.from(downstreamPeers).map(target => (
+                            `${SCtoRCLookup[target]?.[1] || target}`
+                        )).join(",  ")
+                    }</> : ""}
+                    <br /><br />
                     Routes: {receiveUsage[0]} direct / {receiveUsage[1]} forward
                 </Table.Cell>}
                 {!!props.showUpstream && <Table.Cell>
+                    Degree (out): {n.dcnOut.toFixed(4)}<br /><br />
                     Upstream: {upstream.length} {upstream.length > 0 ? <><br />{
                         upstream.map(([target, rtt]) => (
                             `${SCtoRCLookup[target]?.[1] || target}* (${rtt.toFixed(2)}ms)`
@@ -177,6 +246,7 @@ export default function Centrality(props: {
                 <Table.Row>
                     <Table.ColumnHeaderCell>Rank</Table.ColumnHeaderCell>
                     <Table.ColumnHeaderCell>Node</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>Index</Table.ColumnHeaderCell>
                     <Table.ColumnHeaderCell>Centrality</Table.ColumnHeaderCell>
                     {!!props.showDownstream && <Table.ColumnHeaderCell>Downstream (^)</Table.ColumnHeaderCell>}
                     {!!props.showUpstream && <Table.ColumnHeaderCell>Forward (*: upstream)</Table.ColumnHeaderCell>}
